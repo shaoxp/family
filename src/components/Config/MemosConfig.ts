6be2444df69fc2ce9c024643f25d2c9e8a0ce2ad
Ryan Shao <xiaoping.shao@oqton.com>
2023-08-23 09:45:43 +0800

    add some memos

--- src/components/Config/MemosConfig.ts
+++ src/components/Config/MemosConfig.ts
@@ -1,12 +1,16 @@
 export const MemosConfig = {
   memos: [
     {
-      md: "/mds/memos/20230725-诸暨-游侠客2日游.md",
+      md: "/mds/memos/20230708-诸暨-游侠客2日游.md",
       tags: ["travel"]
     },
     {
-      md: "/mds/memos/20230725-诸暨-游侠客2日游.md",
-      tags: ["travel", "event"]
+      md: "/mds/memos/20230630-简单优秀毕业生.md",
+      tags: ["event"]
+    },
+    {
+      md: "/mds/memos/20230623-苏州一日游.md",
+      tags: ["travel"]
     },
   ],
 };
