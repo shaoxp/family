3c95ef812c8b1d7883e2bd27c28837989f2499c8
Ryan Shao <xiaoping.shao@oqton.com>
2024-01-02 17:33:08 +0800

    adjust some order

--- src/components/Config/MemosConfig.ts
+++ src/components/Config/MemosConfig.ts
@@ -1,6 +1,10 @@
 export const MemosConfig = {
   memos: [
     {
+      md: "/mds/memos/20231001-德清-亚运排球之旅.md",
+      tags: ["travel"]
+    },
+    {
       md: "/mds/memos/20230708-诸暨-游侠客2日游.md",
       tags: ["travel"]
     },
@@ -10,10 +14,6 @@ export const MemosConfig = {
     },
     {
       md: "/mds/memos/20230623-苏州一日游.md",
-      tags: ["travel"]
-    },
-    {
-      md: "/mds/memos/20231001-德清-亚运排球之旅.md",
       tags: ["travel"]
     },
   ],
